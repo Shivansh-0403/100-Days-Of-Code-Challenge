f46412444af3627b3c51cc5c4330bfde9827bf5a
Shivansh Srivastava <113630031+Shivansh-0403@users.noreply.github.com>
2023-04-19 13:18:00 +0530

    Update and rename armstrong.cpp to day3_armstrong_number.cpp

--- day3_armstrong_number.cpp
+++ day3_armstrong_number.cpp
@@ -8,14 +8,12 @@ int count(int n){
         digit++;
         n = n/10;
     }
-    cout<<digit<<endl;
+    
     return digit;
 }
 
 int main()
 {
-
-
     int n;
     cin>>n;
 
@@ -24,37 +22,18 @@ int main()
     int sum = 0, rem = 0;
     int num = n;
 
-    // cout<<num<<endl;
     while(n!=0){
         rem = n%10;
         sum += round(pow(rem,digit));
         n = n/10;
     }
-    // cout<<sum<<endl;
+
     if (sum==num){
         cout<<"Armstrong"<<endl;
     }
     else{
         cout<<"Not Armstrong"<<endl;
     }
+    
     return 0;
 }
-
-    // int n;
-    // cout<<"Enter a Number : ";
-    // cin>>n;
-
-    // int number=n;
-    // int cube=0;
-    // while(n>=1){
-    //     int rem = n%10;
-    //     cube = cube + (rem*rem*rem);
-    //     n=n/10;
-    // }
-    // cout<<"Sum of Cubes = "<<cube<<endl;
-    // if(cube==number){
-    //     cout<<"Armstrong Number"<<endl;
-    // }
-    // else{
-    //     cout<<"Not an Armstrong NUmber"<<endl;
-    // }
