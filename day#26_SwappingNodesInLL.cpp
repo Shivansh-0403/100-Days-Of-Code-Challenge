66ab03cd019b7cb51a089857aa8503b2eec39205
Shivansh Srivastava <113630031+Shivansh-0403@users.noreply.github.com>
2024-01-29 17:06:47 +0530

    Create day#26_SwappingNodesInLL.cpp

---
+++ day#26_SwappingNodesInLL.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        if (head==NULL || head->next==NULL){
+            return head;
+        }
+        
+        int size = 0;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while (temp!=NULL){
+            size++;
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if (size == k){
+            swap(head->val, prev->val);
+            return head;
+        }
+
+        int num = size-k+1;
+        temp = head;
+        prev = NULL;
+        ListNode* swap1 = NULL;
+        ListNode* swap2 = NULL;
+
+        for (int i=1; i<=max(num, k); i++){
+            if (i == min(k, num)){
+                swap1 = temp;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+
+        swap2 = prev;
+
+        int val = swap1->val;
+        swap1->val = swap2->val;
+        swap2->val = val;
+
+        return head;
+    }
+};
